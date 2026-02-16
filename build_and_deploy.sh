2d1e36d89f369d067677498e716071f2b0fda54e
Jason Charrier <jason@innovativelanguage.com>
2026-02-15 22:05:18 -0600

    update

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "Building docker image..."
-/usr/local/bin/docker build . --file Dockerfile --tag docker.io/jcharr1/roon-web-controller:latest
+docker build . --file Dockerfile --tag docker.io/jcharr1/roon-web-controller:latest
 
 echo "Pushing to Docker Hub..."
-/usr/local/bin/docker push docker.io/jcharr1/roon-web-controller:latest
+docker push docker.io/jcharr1/roon-web-controller:latest
